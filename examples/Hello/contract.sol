56587dae1cd6aff0c172b4a9613ee5dfff749e48
Tyler Florez <qualiabyte@gmail.com>
2015-02-24 00:21:34 -0800

    Add "Hello Mix" example

---
+++ examples/Hello/contract.sol
@@ -0,0 +1,16 @@
+contract Hello {
+  string32 message;
+
+  // Constructor
+  function Hello() {}
+
+  // Sets the message.
+  function setMessage(string32 _message) {
+    message = _message;
+  }
+
+  // Gets the message.
+  function getMessage() returns (string32 message) {
+    return message;
+  }
+}
